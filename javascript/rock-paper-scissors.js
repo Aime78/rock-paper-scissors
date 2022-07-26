11b70847ef5c784315f8f0ce223fac16b39c134f
bienheureuseuwineza <85676961+Aime78@users.noreply.github.com>
2022-07-26 10:24:50 +0200

    minor changes

--- javascript/rock-paper-scissors.js
+++ javascript/rock-paper-scissors.js
@@ -1,35 +1,33 @@
-
 // Computer Program
 
 function computerPlayer() {
+  // Computer's choices
+  let computer1stChoice = "Rock";
+  let computer2ndChoice = "Paper";
+  let computer3rdChoice = "Scissor";
 
-    // Computer's choices
-    let computer1stChoice = "Rock";
-    let computer2ndChoice = "Paper";
-    let computer3rdChoice = "Scissor";
+  // Randomly generate a number between 0 - 2
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-    // Randomly generate a number between 0 - 2
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-  
-    let randomChoice = getRandomInt(3);
-    // expected output: 0, 1 or 2
+  let randomChoice = getRandomInt(3);
+  // expected output: 0, 1 or 2
 
-    // When the number is 0: return Rock
-    if (randomChoice == 0) {
-        return computer1stChoice;
-    }
+  // When the number is 0: return Rock
+  if (randomChoice == 0) {
+    return computer1stChoice;
+  }
 
-    // When the number is 1: return Paper
-    else if (randomChoice == 1) {
-        return computer2ndChoice;
-    }
+  // When the number is 1: return Paper
+  else if (randomChoice == 1) {
+    return computer2ndChoice;
+  }
 
-    // When the number is 2: return Scissor
-    else if (randomChoice == 2) {
-        return computer3rdChoice;
-    }
+  // When the number is 2: return Scissor
+  else if (randomChoice == 2) {
+    return computer3rdChoice;
+  }
 }
 
 // Scores
@@ -37,28 +35,29 @@ let computerScore = 0;
 let playerScore = 0;
 
 //Game variables
-const endGameBox = document.querySelector('.box');
-const endGameWinner = document.querySelector('.box-title');
+const endGameBox = document.querySelector(".box");
+const endGameWinner = document.querySelector(".box-title");
 
-const restartBtn = document.querySelector('.btn-restart');
+const restartBtn = document.querySelector(".btn-restart");
 
-let gameTitle = document.querySelector('.game-title');
-let paragraph = document.querySelector('.paragraph');
+let gameTitle = document.querySelector(".game-title");
+let paragraph = document.querySelector(".paragraph");
 
-let playerScoreBoard = document.querySelector('.player-report');
+let playerScoreBoard = document.querySelector(".player-report");
 let playerChoiceBoard = playerScoreBoard.firstElementChild;
 
-let computerScoreBoard = document.querySelector('.computer-report');
+let computerScoreBoard = document.querySelector(".computer-report");
 let computerChoiceBoard = computerScoreBoard.firstElementChild;
 
-const playerScoreNumber = document.querySelector('.player');
-const computerScoreNumber = document.querySelector('.computer'); 
+const playerScoreNumber = document.querySelector(".player");
+const computerScoreNumber = document.querySelector(".computer");
 
 // Player choices
-const choices = Array.from(document.querySelectorAll('.choice'));
-choices.forEach(choice => choice.addEventListener('click', function(e){
+const choices = Array.from(document.querySelectorAll(".choice"));
+choices.forEach((choice) =>
+  choice.addEventListener("click", function (e) {
     const element = e.target;
-    const attribute = element.getAttribute('id');
+    const attribute = element.getAttribute("id");
     if (!attribute || playerScore == 5 || computerScore == 5) return;
     const playerSelection = attribute;
     const computerSelection = computerPlayer();
@@ -66,166 +65,162 @@ choices.forEach(choice => choice.addEventListener('click', function(e){
     // Winner of the round
     let roundWinner = playRound(playerSelection, computerSelection);
 
-    if (roundWinner == 'player') {
-        playerScore++;
-        playerScoreNumber.textContent = `Player: ${playerScore}`;
-    }
-    else if (roundWinner == 'computer') {
-        computerScore++;
-        computerScoreNumber.textContent = `Computer: ${computerScore}`;
+    if (roundWinner == "player") {
+      playerScore++;
+      playerScoreNumber.textContent = `Player: ${playerScore}`;
+    } else if (roundWinner == "computer") {
+      computerScore++;
+      computerScoreNumber.textContent = `Computer: ${computerScore}`;
     }
 
     // count each round for 5 rounds total and end the game
 
     if (playerScore == 5) {
-        endGameWinner.textContent = 'You won';
-        endGameBox.classList.add('active');
-        //overlay.classList.add('active')
-        
+      endGameWinner.textContent = "You won";
+      endGameBox.classList.add("active");
+      //overlay.classList.add('active')
+    } else if (computerScore == 5) {
+      endGameWinner.textContent = "You lost";
+      endGameBox.classList.add("active");
+      //overlay.classList.add('active')
     }
-
-    else if (computerScore == 5) {
-        endGameWinner.textContent = 'You lost';
-        endGameBox.classList.add('active');
-        //overlay.classList.add('active')
-    }
-}));
+  })
+);
 
 // Restart
-const playAgain = document.querySelector('.btn-restart');
-playAgain.addEventListener('click', restart);
+const playAgain = document.querySelector(".btn-restart");
+playAgain.addEventListener("click", restart);
 
 // Reset the game
 function restart() {
-    playerScore = 0;
-    computerScore = 0;
+  playerScore = 0;
+  computerScore = 0;
 
-    gameTitle.textContent = 'Choose your weapon';
-    paragraph.textContent = 'First to score 5 points wins the game';
+  gameTitle.textContent = "Choose your weapon";
+  paragraph.textContent = "First to score 5 points wins the game";
 
-    playerChoiceBoard.classList.remove('player-choice');
-    computerChoiceBoard.classList.remove('player-choice');
+  playerChoiceBoard.classList.remove("player-choice");
+  computerChoiceBoard.classList.remove("player-choice");
 
-    playerScoreNumber.textContent = `Player: ${playerScore}`
-    computerScoreNumber.textContent = `Computer: ${computerScore}`
-    endGameBox.classList.remove('active');
+  playerScoreNumber.textContent = `Player: ${playerScore}`;
+  computerScoreNumber.textContent = `Computer: ${computerScore}`;
+  endGameBox.classList.remove("active");
 }
 
 // Playround program
-function playRound (player, computer) {
+function playRound(player, computer) {
+  // Game players' choices
+  let computerChoice = computer;
+  let playerChoice = player;
 
-    // Game players' choices
-    let computerChoice = computer;
-    let playerChoice = player;
+  // Round declarations
 
-    // Round declarations
+  let playerWinner = "player";
+  let computerWinner = "computer";
+  let roundTie = "tie";
 
-    let playerWinner = 'player';
-    let computerWinner = 'computer';
-    let roundTie = 'tie';
+  // Compare the computer's choice against the player's choice
 
-    // Compare the computer's choice against the player's choice
+  // When the computer's choice is rock
+  if (computerChoice.toLowerCase() === "rock") {
+    // When the player's choice is rock: return tie
+    if (playerChoice.toLowerCase() === "rock") {
+      gameTitle.textContent = "Tie!";
+      paragraph.textContent = "Rock ties against rock";
 
-    // When the computer's choice is rock
-    if (computerChoice.toLowerCase() === 'rock') {
-        // When the player's choice is rock: return tie
-        if (playerChoice.toLowerCase() === 'rock') {
-            gameTitle.textContent = 'Tie!';
-            paragraph.textContent = 'Rock ties against rock';
+      playerChoiceBoard.setAttribute("class", "player-choice first");
+      computerChoiceBoard.setAttribute("class", "player-choice first");
 
-            playerChoiceBoard.setAttribute('class', 'player-choice first');
-            computerChoiceBoard.setAttribute('class', 'player-choice first');
+      return roundTie;
+    }
+    // When the player's choice is paper: return player won
+    else if (playerChoice.toLowerCase() === "paper") {
+      gameTitle.textContent = "You won!";
+      paragraph.textContent = "Paper beats rock";
 
-            return roundTie;
-        }
-        // When the player's choice is paper: return player won
-        else if (playerChoice.toLowerCase() === 'paper') {
-            gameTitle.textContent = 'You won!';
-            paragraph.textContent = 'Paper beats rock';
+      playerChoiceBoard.setAttribute("class", "player-choice third");
+      computerChoiceBoard.setAttribute("class", "player-choice first");
 
-            playerChoiceBoard.setAttribute('class', 'player-choice third');
-            computerChoiceBoard.setAttribute('class', 'player-choice first');
+      return playerWinner;
+    }
+    // When the player's choice is scissor: return computer won
+    else if (playerChoice.toLowerCase() === "scissor") {
+      gameTitle.textContent = "Computer won!";
+      paragraph.textContent = "Rock beats scissor";
 
-            return playerWinner;
-        }
-        // When the player's choice is scissor: return computer won
-        else if (playerChoice.toLowerCase() === 'scissor') {
-            gameTitle.textContent = 'Computer won!';
-            paragraph.textContent = 'Rock beats scissor';
+      playerChoiceBoard.setAttribute("class", "player-choice second");
+      computerChoiceBoard.setAttribute("class", "player-choice first");
 
-            playerChoiceBoard.setAttribute('class', 'player-choice second');
-            computerChoiceBoard.setAttribute('class', 'player-choice first');
+      return computerWinner;
+    }
+  }
 
-            return computerWinner;
-        }
+  // When the computer's choice is paper
+  else if (computerChoice.toLowerCase() === "paper") {
+    // When the player's choice is rock: return computer won
+    if (playerChoice.toLowerCase() === "rock") {
+      gameTitle.textContent = "Computer won!";
+      paragraph.textContent = "Paper beats rock";
+
+      playerChoiceBoard.setAttribute("class", "player-choice first");
+      computerChoiceBoard.setAttribute("class", "player-choice third");
+
+      return computerWinner;
+    }
+    // When the player's choice is paper: return tie
+    else if (playerChoice.toLowerCase() === "paper") {
+      gameTitle.textContent = "Tie!";
+      paragraph.textContent = "Paper ties against paper";
+
+      playerChoiceBoard.setAttribute("class", "player-choice third");
+      computerChoiceBoard.setAttribute("class", "player-choice third");
+
+      return roundTie;
+    }
+    // When the player's choice is scissor: return player won
+    else if (playerChoice.toLowerCase() === "scissor") {
+      gameTitle.textContent = "You won!";
+      paragraph.textContent = "scissor beats paper";
+
+      playerChoiceBoard.setAttribute("class", "player-choice second");
+      computerChoiceBoard.setAttribute("class", "player-choice third");
+
+      return playerWinner;
+    }
+  }
+
+  // When the computer's choice is scissor
+  else if (computerChoice.toLowerCase() === "scissor") {
+    // When the player's choice is rock: return player won
+    if (playerChoice.toLowerCase() === "rock") {
+      gameTitle.textContent = "You won!";
+      paragraph.textContent = "Rock beats scissor";
+
+      playerChoiceBoard.setAttribute("class", "player-choice first");
+      computerChoiceBoard.setAttribute("class", "player-choice second");
+
+      return playerWinner;
+    }
+    // When the player's choice is paper: return computer won
+    else if (playerChoice.toLowerCase() === "paper") {
+      gameTitle.textContent = "Computer won!";
+      paragraph.textContent = "scissor beats paper";
+
+      playerChoiceBoard.setAttribute("class", "player-choice third");
+      computerChoiceBoard.setAttribute("class", "player-choice second");
+
+      return computerWinner;
     }
 
-    // When the computer's choice is paper
-    else if (computerChoice.toLowerCase() === 'paper') {
-        // When the player's choice is rock: return computer won 
-        if (playerChoice.toLowerCase() === 'rock') {
-            gameTitle.textContent = 'Computer won!';
-            paragraph.textContent = 'Paper beats rock'
+    // When the player's choice is scissor: return tie
+    else if (playerChoice.toLowerCase() === "scissor") {
+      gameTitle.textContent = "Tie!";
+      paragraph.textContent = "Scissor ties against scissor";
 
-            playerChoiceBoard.setAttribute('class', 'player-choice first');
-            computerChoiceBoard.setAttribute('class', 'player-choice third');
+      playerChoiceBoard.setAttribute("class", "player-choice second");
+      computerChoiceBoard.setAttribute("class", "player-choice second");
 
-            return computerWinner;
-        }
-        // When the player's choice is paper: return tie
-        else if (playerChoice.toLowerCase() === 'paper') {
-            gameTitle.textContent = 'Tie!';
-            paragraph.textContent = 'Paper ties against paper'
-
-            playerChoiceBoard.setAttribute('class', 'player-choice third');
-            computerChoiceBoard.setAttribute('class', 'player-choice third');
-
-            return roundTie;
-        }
-        // When the player's choice is scissor: return player won
-        else if (playerChoice.toLowerCase() === 'scissor') {
-            gameTitle.textContent = 'You won!';
-            paragraph.textContent = 'scissor beats paper'
-
-            playerChoiceBoard.setAttribute('class', 'player-choice second');
-            computerChoiceBoard.setAttribute('class', 'player-choice third');
-
-            return playerWinner;
-        }
+      return roundTie;
     }
-
-    // When the computer's choice is scissor
-    else if (computerChoice.toLowerCase() === 'scissor') {
-        // When the player's choice is rock: return player won
-        if (playerChoice.toLowerCase() === 'rock') {
-            gameTitle.textContent = 'You won!';
-            paragraph.textContent = 'Rock beats scissor'
-
-            playerChoiceBoard.setAttribute('class', 'player-choice first');
-            computerChoiceBoard.setAttribute('class', 'player-choice second');
-
-            return playerWinner;
-        }
-        // When the player's choice is paper: return computer won
-        else if (playerChoice.toLowerCase() === 'paper') {
-            gameTitle.textContent = 'Computer won!';
-            paragraph.textContent = 'scissor beats paper'
-
-            playerChoiceBoard.setAttribute('class', 'player-choice third');
-            computerChoiceBoard.setAttribute('class', 'player-choice second');
-
-            return computerWinner;
-        }
-
-        // When the player's choice is scissor: return tie
-        else if (playerChoice.toLowerCase() === 'scissor') {
-            gameTitle.textContent = 'Tie!';
-            paragraph.textContent = 'Scissor ties against scissor'
-
-            playerChoiceBoard.setAttribute('class', 'player-choice second');
-            computerChoiceBoard.setAttribute('class', 'player-choice second');
-
-            return roundTie;
-        }
-    }
+  }
 }
